db246b3b8a6ae27ea31640008686fc8270e365c5
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2017-07-20 19:34:21 +0300

    Added version arguments.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import chalk from 'chalk';
 import commandLineCommands from 'command-line-commands';
+import minimist from 'minimist';
 
+import packageFile from '../package.json';
 import merge from './merge';
-import translate from './translate';
 
-const validCommands = [null, 'merge', 'translate'];
+const validCommands = [null, 'merge'/* , 'translate' */];
 
 const {
   command,
@@ -12,15 +13,18 @@ const {
 } = commandLineCommands(validCommands);
 
 if (command === null) {
+  const options = minimist(argv);
+  if (options.version || options.v) {
+    console.log(chalk`{green Version: ${packageFile.version}}`);
+    process.exit(0);
+  }
+
   console.log(chalk`{red Valid commands:}
     {blue ${validCommands.splice(1).join(', ')}}`);
   process.exit(1);
 }
 
-if (command === merge) {
-
-}
-
-if (command === translate) {
-
+if (command === 'merge') {
+  const options = minimist(argv);
+  console.log(options);
 }
